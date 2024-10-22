80eb2eb60550ba01618c6cbdceaf819cc1a34c6d
limda00 <dylim7715@gmail.com>
2024-10-22 21:36:06 +0900

    🎉 :: ReactQuery 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import { GlobalStyle } from './Style/GlobalStyle';
+import "./fonts/font.css"
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      staleTime: 30000,
+      refetchInterval: 0,
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient} >
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
