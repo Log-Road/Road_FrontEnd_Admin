46e773fad9e1d9e2831bf88ea3586be985abcd6b
limda00 <dylim7715@gmail.com>
2024-10-23 10:39:04 +0900

    💄 :: 레이아웃 생성

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "../Components/Header";
+import Layout from "../Components/Layout";
+import ClubManage from "../Pages/ClubManage";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Header />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="/club" element={<ClubManage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
