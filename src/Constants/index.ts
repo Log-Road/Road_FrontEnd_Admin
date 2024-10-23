4ed0c85f995514f4eb70d8f7f8f510d6d952b555
limda00 <dylim7715@gmail.com>
2024-10-23 10:40:00 +0900

    🎨 :: Footer 상수 값 추가

--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -1,1 +1,7 @@
-export {}
+import { FooterInformationType } from "../Models"
+
+export const FooterInformation: FooterInformationType[] = [
+  {title: "DSM을 위한 프로젝트 저장소", info: "BackEnd 유나현 | FrontEnd 김호선 박예빈 임다영"},
+  {title: "주소", info: "대전광역시 유성구 가정북로 76"},
+  {title: "문의", info: "dsm.log@dsm.hs.kr"}
+]
