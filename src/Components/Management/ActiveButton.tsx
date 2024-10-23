8853afef400d6df77d1a0692edbaafb6ae00a9fa
limda00 <dylim7715@gmail.com>
2024-10-23 10:37:58 +0900

    💄 :: 테이블 값 조작 버튼 컴포넌트 생성

---
+++ src/Components/Management/ActiveButton.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+import { color } from "../../Style"
+
+interface ButtonProps {
+  text?: string,
+  active?: boolean,
+  onClick?: () => void
+}
+
+export default function ActiveButton({ text, active = false, onClick }: ButtonProps) {
+  return (
+    <Button active={active} onClick={onClick}>
+      {text}
+    </Button>
+  )
+}
+
+const Button = styled.button<{ active: boolean }>`
+padding: 4px 20px;
+border-radius: 8px;
+border: 1px solid ${({ active }) => active ? color.gray[300] : color.gray[100]};
+background-color: ${({ active }) => active ? color.white : color.gray[100]};
+`
