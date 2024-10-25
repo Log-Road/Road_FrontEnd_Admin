073ecb5eb996ec7e5cd6dd090795adebc356af6d
limda00 <dylim7715@gmail.com>
2024-10-25 15:50:55 +0900

    ✨ ::  동아리 개별 추가 API

--- src/Utils/api/Club/index.ts
+++ src/Utils/api/Club/index.ts
@@ -2,6 +2,7 @@ import { MutateOptions, useMutation, useQuery } from 'react-query';
 import instance from '@/Utils/axios';
 import { AxiosError } from 'axios';
 import { clubListDefaultType } from '@/Models/ClubList';
+import ApiError from '@/Utils/axios/ApiError';
 
 const path = '/club'
 
@@ -12,8 +13,40 @@ const path = '/club'
  */
 
 export const useGetAllClubList = () => {
+  const { handleError } = ApiError()
+  
   return useQuery(['getAllClubList'], async() => {
-    const { data } = await instance.get<clubListDefaultType>(`${path}`)
-    return data
+    try {
+      const { data } = await instance.get<clubListDefaultType>(`${path}`)
+      return data
+    } catch(error) {
+      handleError(error)
+    }
   })
+}
+
+/**
+ * 동아리 개별 추가 API
+ * @params
+ * @returns 동아리 ID
+ */
+
+export const useAddClub = (isActive: boolean, clubName: string) => {
+  const { handleError } = ApiError()
+  
+  return useMutation(
+    async () => {
+      try {
+        const response = await instance.post(`${path}`,{
+          isActive,
+          clubName
+      })
+        return response.data
+      } catch(error: any) {
+        if(error.response?.status === 409) {
+          console.log("동아리 이름이 중복되었습니다")
+        }
+        handleError(error)
+      }}
+  )
 }
