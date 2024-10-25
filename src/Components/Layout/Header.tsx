7e7ac14e7b44ecc9389fd0ef4565f02178616de2
limda00 <dylim7715@gmail.com>
2024-10-26 02:11:53 +0900

    💄 :: 헤더 요소 키 지정

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -11,7 +11,9 @@ export default function Header() {
       <LogoAndListWrap>
         <Logo>ROAD</Logo>
         <ListWrap>
-          {list.map(value => <Text>{value}</Text>)}
+          {list.map((value) =>
+            <Text key={value}>{value}</Text>
+          )}
         </ListWrap>
       </LogoAndListWrap>
       <ListWrap>
