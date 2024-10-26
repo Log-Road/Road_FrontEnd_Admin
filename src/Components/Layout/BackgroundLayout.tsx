27e74e28abe7632547a3ca64032780267102baf4
limda00 <dylim7715@gmail.com>
2024-10-26 17:31:40 +0900

    💄 :: 배경 유무 레이아웃 지정

---
+++ src/Components/Layout/BackgroundLayout.tsx
@@ -0,0 +1,51 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import Modal from "@/Components/Common/Modal";
+import { useModal } from "@/Context/ModalContext";
+import BackgroundImage from "@/Assets/BackgroundImage.svg"
+
+export default function DefaultLayout() {
+  const { isOpen } = useModal()
+
+  return (
+    <Container>
+      <Header />
+      <Content>
+        <BackgroundImageWrap />
+        <Outlet />
+      </Content>
+      <Footer />
+      {isOpen && <Modal />}
+    </Container>
+  )
+}
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+position: relative;
+background-color: #F6F6F6;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const Content = styled.div`
+display: flex;
+justify-content: center;
+min-height: calc(100vh - 248px);
+`
+
+const BackgroundImageWrap = styled.img`
+position: absolute;
+top: 90px;
+left: 0;
+width: 100%;
+height: 55%;
+background-image: url(${BackgroundImage});
+background-size: cover;
+background-position: center;
+z-index: 0;
+`
--- src/Components/Layout/DefaultLayout.tsx
+++ src/Components/Layout/DefaultLayout.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import Modal from "@/Components/Common/Modal";
 import { useModal } from "@/Context/ModalContext";
 
-export default function Layout() {
+export default function DefaultLayout() {
   const { isOpen } = useModal()
 
   return (
