01d89c4492e2da93474e55c09f6258d8cc1582fa
limda00 <dylim7715@gmail.com>
2024-10-26 22:27:49 +0900

    💄 :: 프로젝트 승인 및 반려 페이지 퍼블리싱

---
+++ src/Pages/PostApproval/index.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components"
+import Write from "@/Pages/PostApproval/Write"
+import { Status } from "@/Assets"
+import { color, font } from "@/Styles"
+import Approve from "@/Components/PostCard/Approve"
+import Refuse from "@/Components/PostCard/Refuse"
+
+const PostApproval = () => {
+  return (
+    <Container>
+      <Write />
+      <SelectType>
+        <ProjectStatus>
+          <Status color={color.gray[700]} />
+          <Text>승인대기</Text>
+        </ProjectStatus>
+        <Approve />
+        <Refuse type="Refuse" />
+      </SelectType>
+    </Container>
+  )
+}
+
+export default PostApproval
+
+const Container = styled.div`
+width: 920px;
+display: flex;
+justify-content: space-between;
+`
+
+const SelectType = styled.div`
+display: flex;
+flex-direction: column;
+gap: 16px;
+`
+
+const ProjectStatus = styled.span`
+display: flex;
+gap: 8px;
+padding: 8px 16px;
+align-items: center;
+border-radius: 20px;
+background-color: ${color.white};
+`
+
+const Text = styled.p`
+${font.medium14}
+color: ${color.gray[700]};
+`
