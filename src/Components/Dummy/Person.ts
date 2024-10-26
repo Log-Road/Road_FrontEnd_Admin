7c07039553f04171f59a6ac9dc50271c5de61570
limda00 <dylim7715@gmail.com>
2024-10-26 14:43:01 +0900

    📝 :: 학생 더미 값 추가

---
+++ src/Components/Dummy/Person.ts
@@ -0,0 +1,28 @@
+import { PersonListDefaultType } from "@/Models/Manage"
+
+export const Person: PersonListDefaultType = {
+  arr: [
+    {
+      "personId" : 1,
+      "grade" : 1,
+      "class" : 2,
+      "number" : 10,
+      "name" : "홍길동전",
+      "status" : "ENROLL"
+    }, {
+      "personId" : 2,
+      "grade" : 2,
+      "class" : 1,
+      "number" : 12,
+      "name" : "홍길서",
+      "status" : "ENROLL"
+    }, {
+      "personId" : 3,
+      "grade" : 3,
+      "class" : 2,
+      "number" : 14,
+      "name" : "홍길남",
+      "status" : "ENROLL"
+    }
+  ]
+}
