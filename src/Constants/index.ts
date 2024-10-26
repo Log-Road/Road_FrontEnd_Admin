20d2de419f80b7aea79adfe2fd0999c93473f861
limda00 <dylim7715@gmail.com>
2024-10-26 14:43:31 +0900

    💄 :: 학생 검색 컴포넌트 추가

--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -1,7 +1,13 @@
-import { FooterInformationType } from "@/Models"
+import { FooterInformationType, SearchItemsType } from "@/Models"
 
 export const FooterInformation: FooterInformationType[] = [
   {title: "DSM을 위한 프로젝트 저장소", info: "BackEnd 유나현 | FrontEnd 김호선 박예빈 임다영"},
   {title: "주소", info: "대전광역시 유성구 가정북로 76"},
   {title: "문의", info: "dsm.log@dsm.hs.kr"}
 ]
+
+export const SearchItems: SearchItemsType[] = [
+  { label: "학년", text: "학년 선택하기", options: ['1', '2', '3'] },
+  { label: "반", text: "반 선택하기", options: ['1', '2', '3', '4'] },
+  { label: "학년", text: "학년 선택하기", options: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'] },
+];
