a17eb929a6c54cae512815fcf82380f8848e0246
limda00 <dylim7715@gmail.com>
2024-10-27 13:57:33 +0900

    ✨ :: 대회 디테일 조회 API

---
+++ src/Utils/api/Contest/index.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import instance from '@/Utils/axios';
+import { ClubListDefaultType, ClubType } from '@/Models/Manage';
+import ApiError from '@/Utils/axios/ApiError';
+import toast from 'react-hot-toast';
+import { ContestDetailType } from "@/Models/Manage";
+
+const path = '/competition'
+
+/**
+ * 대회 디테일 조회 API
+ * @params id
+ * @returns 대회 정보 Data
+ */
+
+export const useGetContestDetail = (id: string) => {
+  const { handleError } = ApiError();
+
+  return useQuery<ContestDetailType, Error>({
+    queryKey: ["ContestDetail", id],
+    queryFn: async () => {
+      try {
+        const response = await instance.get(`${path}/inform/${id}`); // URL에서 id를 사용
+        return response.data;
+      } catch (error) {
+        handleError(error);
+        throw error;
+      }
+    },
+    enabled: !!id
+  });
+};
