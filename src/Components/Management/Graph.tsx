fc2e30bb73ce2420ee2103e218b83fb26a10699e
limda00 <dylim7715@gmail.com>
2024-10-28 11:00:09 +0900

    💄 :: 원형 그래프 생성

--- src/Components/Management/Graph.tsx
+++ src/Components/Management/Graph.tsx
@@ -1,5 +1,56 @@
-export default function Graph() {
-  return (
-    <></>
-  )
+import ApexCharts from 'apexcharts';
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+import * as ReactDOM from 'react-dom';
+
+interface ApexChartProps { }
+
+interface ApexChartState {
+  series: number[];
+  options: ApexCharts.ApexOptions;
 }
+
+class ApexChart extends React.Component<ApexChartProps, ApexChartState> {
+  constructor(props: ApexChartProps) {
+    super(props);
+
+    this.state = {
+      series: [70],
+      options: {
+        chart: {
+          height: 350,
+          type: 'radialBar',
+        },
+        plotOptions: {
+          radialBar: {
+            hollow: {
+              size: '70%',
+            },
+          },
+        },
+        labels: ['Cricket'],
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="chart">
+          <ReactApexChart
+            options={this.state.options}
+            series={this.state.series}
+            type="radialBar"
+            height={350}
+          />
+        </div>
+        <div id="html-dist"></div>
+      </div>
+    );
+  }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(<ApexChart />, domContainer);
+
+export default ApexChart;
