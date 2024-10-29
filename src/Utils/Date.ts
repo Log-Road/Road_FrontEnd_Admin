943c72b055a71ebbe06c64b9b880e5c5642640a9
limda00 <dylim7715@gmail.com>
2024-10-29 22:43:43 +0900

    ✨ :: UTC 변환 함수 생성

--- src/Utils/Date.ts
+++ src/Utils/Date.ts
@@ -4,6 +4,12 @@ export const covertISOtoKST = (date: string) => {
 }
 
 export const convertKSTtoUTC = (date: Date): string => {
-  const utcDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-  return utcDate.toISOString().split('T')[0];
-};
+  const utcYear = date.getUTCFullYear();
+  const utcMonth = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const utcDate = String(date.getUTCDate()).padStart(2, '0');
+  const utcHours = String(date.getUTCHours()).padStart(2, '0');
+  const utcMinutes = String(date.getUTCMinutes()).padStart(2, '0');
+  const utcSeconds = String(date.getUTCSeconds()).padStart(2, '0');
+
+  return `${utcYear}-${utcMonth}-${utcDate}T${utcHours}:${utcMinutes}:${utcSeconds}Z`;
+}
