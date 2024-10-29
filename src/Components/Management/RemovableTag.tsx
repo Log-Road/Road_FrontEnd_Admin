6c47c6ec45a429d17c4a34ab7bfed375913ac9c1
limda00 <dylim7715@gmail.com>
2024-10-29 17:36:57 +0900

    💄 :: 태그 요소 타입 변경

--- src/Components/Management/RemovableTag.tsx
+++ src/Components/Management/RemovableTag.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components"
 
 interface TagPropsType {
   text?: string,
-  count?: string
+  count?: number
 }
 
 export default function RemovableTag({ text, count }: TagPropsType) {
