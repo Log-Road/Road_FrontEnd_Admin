21b5a5f8b7710d836d21abb5993897baed7ee632
limda00 <dylim7715@gmail.com>
2024-11-01 09:04:29 +0900

    📝 :: 프로젝트 더미 값 추가

---
+++ src/Components/Dummy/Project.ts
@@ -0,0 +1,49 @@
+const Project = {
+  "list" : [
+    {
+      "id" : "fadsgfds",
+      "status" : "PENDING",
+      "name" : "펜딩",
+      "image" : "https://blog.kakaocdn.net/dn/0mySg/btqCUccOGVk/nQ68nZiNKoIEGNJkooELF1/img.jpg",
+      "authorCategory" : "PERSONAL",
+      "member" : [
+        "ㅇㄹㅁㄴ",
+        "ㅇㅎㄴ",
+        "ㅁㄴㅇㄹㅎ"
+      ],
+      "introduction" : "ㅁㄴㅇㅎㄹ오",
+      "createdAt" : "20240101T03:25:78Z",
+      "like" : 1
+    },
+    {
+      "id" : "ㅇㄹㄴㅎㅁㄹ",
+      "status" : "APPROVAL",
+      "name" : "ㄹㅇㄴㅎ",
+      "image" : "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/14Fa/image/joib7vCDm4iIP7rNJR2ojev0A20.jpg",
+      "authorCategory" : "PERSONAL",
+      "member" : [
+        "ㄹㄴㅇ",
+        "ㄹㄴㅁㅎ",
+        "ㄹㄴㅇ"
+      ],
+      "introduction" : "ㄴㅇㅁㄹㅎㄹ옿ㄹ허",
+      "createdAt" : "20240101T03:25:78Z",
+      "like" : 1
+    },
+    {
+      "id" : "ㄴㅇㅁㅎ",
+      "status" : "APPROVAL",
+      "name" : "ㄹㅁㅎㅇㄴ",
+      "image" : "https://img.freepik.com/free-photo/assortment-with-happy-emotion_23-2148860256.jpg",
+      "authorCategory" : "PERSONAL",
+      "member" : [
+        "ㅇㅎㅁㄹㅇ",
+        "ㅎㄹㅁㅇ",
+        "ㅇㄹㄴㅁ"
+      ],
+      "introduction" : "ㅇㄹㄴ",
+      "createdAt" : "20240101T03:25:78Z",
+      "like" : 1
+    },
+  ]
+}
